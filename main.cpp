cf35ebde85c0b8df0efffe235f6cd31aface97fa
lulliloo-ui <madieva.tv@edu.spbstu.ru>
2025-10-25 12:40:09 +0300

    Initial commit

--- main.cpp
+++ main.cpp
@@ -1,3 +1,15 @@
+#include<iostream>
 int main()
 {
+  using u_t = unsigned;
+  u_t a = 0;
+  std::cin >> a;
+  if(std::sin.eof()){
+    std::cout << 0;
+    std::cout << "\n";
+  }
+  else if(std::cin.fail()){
+    std::cerr << "Error\n";
+    return 1;
+  }
 }
